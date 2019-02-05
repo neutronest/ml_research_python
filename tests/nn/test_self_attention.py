9a84e67f83af252242a994e4f93ff57621519c80
neutronest <neutronest@gmail.com>
2019-02-05 12:47:54 +0800

    [@neutronest] refactor tests

--- tests/nn/test_self_attention.py
+++ tests/nn/test_self_attention.py
@@ -4,21 +4,33 @@ import torch.nn as nn
 
 from research.nn.self_attention import NaiveMultiHeadSelfAttention
 
+batch_size = 2
+n_head = 3
+seq_length = 5
+embedding_size = 7
+hidden_size = 11
+
+
 def test_query_weight_shape():
 
-    batch_size = 2
-    n_head = 3
-    seq_length = 5
-    embedding_size = 7
-    hidden_size = 11
     multi_head_self_attention_machine = NaiveMultiHeadSelfAttention(
         embedding_size,
         hidden_size,
         n_head
     )
     input_x = torch.randn(batch_size, seq_length, embedding_size)
-    # assert(multi_head_self_attention_machine.w_queries(input_x).shape \
-    #     == torch.Size([batch_size, n_head * hidden_size]))
+    query_vector = multi_head_self_attention_machine.w_queries(input_x)
+    assert(query_vector.shape \
+        == torch.Size([batch_size, seq_length, n_head * hidden_size]))
     
+   
+
+def test_naive_multi_head_self_attention_forward():
+    multi_head_self_attention_machine = NaiveMultiHeadSelfAttention(
+        embedding_size,
+        hidden_size,
+        n_head
+    )
+    input_x = torch.randn(batch_size, seq_length, embedding_size)
     output = multi_head_self_attention_machine.forward(input_x)
     assert(output.size() == torch.Size([batch_size, n_head, seq_length, hidden_size]))
