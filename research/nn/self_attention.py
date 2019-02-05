926e86f63822b91ec217880642710c338daf6f2e
neutronest <neutronest@gmail.com>
2019-02-05 09:50:54 +0800

    [@neutronest] first forward commit

--- research/nn/self_attention.py
+++ research/nn/self_attention.py
@@ -18,8 +18,24 @@ class MultiHeadSelfAttention(nn.Module):
         
         self.w_queries = nn.Linear(input_size, n_head * hidden_size)
         self.w_keyes = nn.Linear(input_size, n_head * hidden_size)
-        self.w_values = nn.Linear(input_size, n_head, hidden_size)
+        self.w_values = nn.Linear(input_size, n_head * hidden_size)
 
-        nn.init.normal_(self.w_queries, mean=0, std=np.sqrt(2.0 / 100))
+        nn.init.normal_(self.w_queries.weight, mean=0, std=np.sqrt(2.0 / (input_size+hidden_size)))
+        nn.init.normal_(self.w_keyes.weight, mean=0, std=np.sqrt(2.0 / (input_size+hidden_size)))
+        nn.init.normal_(self.w_values.weight, mean=0, std=np.sqrt(2.0 / (input_size+hidden_size)))        
+        return
+    
+    def forward(self, input_x, mask=None):
+        """
+        """
+
+        n_batch, len_of_sequence, _ = input_x.size()
+        
+        query_vector = self.w_queries(input_x)
+        key_vector = self.w_keyes(input_x)
+        value_vector = self.w_values(input_x)
+        query_vector = query_vector.view(n_batch, len_of_sequence, self.n_head, self.hidden_size)
+        key_vector = key_vector.view(n_batch, len_of_sequence, self.n_head, self.hidden_size)
+        value_vector = value_vector.view(n_batch, len_of_sequence, self.n_head, self.hidden_size)
         
         return
--- tests/nn/test_self_attention.py
+++ tests/nn/test_self_attention.py
@@ -6,14 +6,18 @@ from research.nn.self_attention import MultiHeadSelfAttention
 
 def test_query_weight_shape():
 
-    input_size = 4
+    sequence_size = 4
     hidden_size = 8
+    embedding_size = 10
     n_head = 2
     batch_size = 10
     multi_head_self_attention_machine = MultiHeadSelfAttention(
-        input_size,
+        embedding_size,
         hidden_size,
         n_head
     )
-    input_x = torch.randn(batch_size, input_size)
-    assert(multi_head_self_attention_machine.w_query(input_x).shape == torch.Size([batch_size, n_head * hidden_size]))
+    input_x = torch.randn(batch_size, sequence_size, embedding_size)
+    # assert(multi_head_self_attention_machine.w_queries(input_x).shape \
+    #     == torch.Size([batch_size, n_head * hidden_size]))
+    
+    multi_head_self_attention_machine.forward(input_x)
